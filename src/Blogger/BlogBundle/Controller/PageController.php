f82511f52160dbc71ff2df0ad562e2c9da71cafd
hardik-vi <hardikkotak.virtueinfo@gmail.com>
2012-06-18 16:23:48 +0530

    initial commit

---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,71 @@
+<?php
+// src/Blogger/BlogBundle/Controller/PageController.php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// Import new namespaces
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+use Symfony\Component\HttpFoundation\Request;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:index.html.twig');
+    }
+    
+     public function aboutAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:about.html.twig');
+    }
+    
+    public function contactAction()
+    {
+         $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+                     $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('hardikkotak.virtueinfo@gmail.com')
+                    ->setTo('hardik.kotak004@gmail.com')
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+                $this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+        //$request = $this->getRequest();
+     }
+     
+      /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+    }
+}
